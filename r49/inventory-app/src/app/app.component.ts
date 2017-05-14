68a7fd3bf546b3158e02e947a404ce74278b4f4e
Edwin Lunandy <elunandy@newsignature.com>
2017-05-14 12:59:29 -0400

    First pass on new app-root.

---
+++ r49/inventory-app/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import {Component} from '@angular/core';
+
+class Product {
+  constructor(public sku: string,
+              public name: string,
+              public imageUrl: string,
+              public department: string[],
+              public price: number) {
+
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  product: Product[];
+
+  constructor() {
+
+    this.product = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/resources/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/resources/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/resources/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+    ]
+  }
+
+
+}
